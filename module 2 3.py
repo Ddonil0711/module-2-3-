373fd6f50d46168dbdbf8b16077239fbf6cf1d81
Ddonil0711 <danil.kremnev.00@mail.ru>
2024-07-29 10:59:27 +0300

    Delete module 2 3.py

--- module 2 3.py
+++
@@ -1,8 +0,0 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-a = 0
-while True:
-    number = int(input('введите число: '))
-    if a < len(my_list) and number > 0:
-        print(number)
-    else:
-        break
